7bbf5628b4ca39a17290fb847b4e001b755993cb
Vladimir Espinola <vladimir.espinola@gmail.com>
2018-01-10 17:35:53 -0300

    some changes

--- VirtualTourist/Controller/MapViewController.swift
+++ VirtualTourist/Controller/MapViewController.swift
@@ -84,27 +84,33 @@ extension MapViewController: MKMapViewDelegate {
     }
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-
-        selectedAnnotation = view.annotation
         
-        mapView.deselectAnnotation(view.annotation, animated: true)
         
-        let bbox = Util.getBoundingBox(for: selectedAnnotation.coordinate.latitude, and: selectedAnnotation.coordinate.longitude)
-        
-        let performToDetailViewController: (MKAnnotation, [FlickrPhoto]) -> Void = {  annotation, photos in
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let pinDetailViewController = storyboard.instantiateViewController(withIdentifier: "PinDetailViewControllerID") as! PINDetailViewController
-            pinDetailViewController.selectedAnnotation = annotation
-            pinDetailViewController.photos = photos
-            pinDetailViewController.bbox = bbox
+        if currentEditState == .editing {
+//            mapView.removeAnnotation(view.annotation!).
+        } else {
+            selectedAnnotation = view.annotation
             
-            self.navigationController?.pushViewController(pinDetailViewController, animated: true)
+            mapView.deselectAnnotation(view.annotation, animated: true)
             
+            let bbox = Util.getBoundingBox(for: selectedAnnotation.coordinate.latitude, and: selectedAnnotation.coordinate.longitude)
+            
+            let performToDetailViewController: (MKAnnotation, [FlickrPhoto]) -> Void = {  annotation, photos in
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let pinDetailViewController = storyboard.instantiateViewController(withIdentifier: "PinDetailViewControllerID") as! PINDetailViewController
+                pinDetailViewController.selectedAnnotation = annotation
+                pinDetailViewController.photos = photos
+                pinDetailViewController.bbox = bbox
+                
+                self.navigationController?.pushViewController(pinDetailViewController, animated: true)
+                
+            }
+            
+            FlickrHandler.shared().getPhotos(with: bbox, in: self, onCompletion: { photos in
+                performToDetailViewController(self.selectedAnnotation, photos)
+            })
         }
-        
-        FlickrHandler.shared().getPhotos(with: bbox, in: self, onCompletion: { photos in
-            performToDetailViewController(self.selectedAnnotation, photos)
-        })
+
     }
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
